d465c50f163320fb9bcad601233e4f108e81c7f4
Raymond Shiner <raymondshiner@gmail.com>
2019-01-10 14:56:17 -0800

    Init Commit - make sure build is working properly

---
+++ SecretSanta/test/SecretSanta.Domain.Tests/Models/UserTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Domain.Models;
+
+namespace SecretSanta.Domain.Tests.Models
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void CreateUser()
+        {
+            //Arrange
+            User user = new User {FirstName = "Inigo", LastName = "Montoya"};
+            Assert.AreEqual("Inigo", user.FirstName);
+
+            //Act
+
+
+            //Assert
+
+        }
+
+    }
+}
