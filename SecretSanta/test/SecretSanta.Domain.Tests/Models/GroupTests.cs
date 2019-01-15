85a710755ebba41d0d3e2ddd78cac1121ee8958b
Bryan caesar <bryancaesar@eagles.ewu.edu>
2019-01-15 01:16:05 -0800

    Unit tests done for Group and Message.

---
+++ SecretSanta/test/SecretSanta.Domain.Tests/Models/GroupTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecretSanta.Domain.Tests.Models
+{
+    [TestClass]
+    public class GroupTests
+    {
+        [TestMethod]
+        public void DefaultConstructor_ReturnsGroupObject()
+        {
+            Group group = new Group();
+            Assert.IsTrue(group is Group);
+        }
+
+        [TestMethod]
+        public void DefaultConstructor_InheritsFromEntity()
+        {
+            Group group = new Group();
+            Assert.IsTrue(group is Entity);
+        }
+
+        [TestMethod]
+        public void SetAndGetTitle_ReturnsSetTitle()
+        {
+            Group group = new Group();
+            string expectedTitle = "No one expects the spanish inquisition!";
+            group.Title = expectedTitle;
+            Assert.AreEqual(expectedTitle, group.Title);
+        }
+
+        [TestMethod]
+        public void SetAndGetID_ReturnsSetID()
+        {
+            Group group = new Group();
+            int expectedID = 42;
+            group.Id = expectedID;
+            Assert.AreEqual(expectedID, group.Id);
+        }
+
+        [TestMethod]
+        public void SetUsers_SetsSuccessfully()
+        {
+            Group group = new Group();
+            List<User> userList = new List<User>();
+            group.Users = userList;
+            Assert.AreEqual(userList, group.Users);
+        }
+    }
+}
