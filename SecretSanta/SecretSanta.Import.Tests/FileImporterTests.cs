3c0b9f8042896640acbe0075c6cd9ac39414e94b
Raymond Shiner <raymondshiner@gmail.com>
2019-01-21 23:03:34 -0800

    Done with Everything, TDD utilized for everything.

---
+++ SecretSanta/SecretSanta.Import.Tests/FileImporterTests.cs
@@ -0,0 +1,302 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Domain.Models;
+
+namespace SecretSanta.Import.Tests
+{
+    [TestClass]
+    public class FileImporterTests
+    {
+        //For the purpose of this testing class, let abbreviations FNF and LNF be
+        //"FirstName First" and "LastName First" accordingly.
+        
+        private FileImporter Importer { get; set; }
+
+        [TestInitialize]
+        public void InitializeTests()
+        {
+            Initialize_ValidTestFile1_RayFNF();
+            Initialize_ValidTestFile2_MarkFNF();
+            Initialize_ValidTestFile3_MikeLNF();
+            Initialize_InvalidTestFile1_GarbageHeader();
+            Importer = new FileImporter();
+        }
+
+        private void Initialize_ValidTestFile1_RayFNF()
+        {
+            string path = System.Environment.CurrentDirectory;
+            path += @"\ValidFile1_RayFNF.txt";
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Name: Raymond Shiner");
+                    sw.WriteLine("PS4");
+                    sw.WriteLine("Money");
+                    sw.WriteLine("Peace on Earth");
+                }
+            }
+        }
+        
+        private void Initialize_ValidTestFile2_MarkFNF()
+        {
+            string path = System.Environment.CurrentDirectory;
+            path += @"\ValidFile2_MarkFNF.txt";
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Name: Mark Michaelis");
+                    sw.WriteLine("Fame");
+                    sw.WriteLine("Fortune");
+                    sw.WriteLine("Abs");
+                }
+            }
+        }
+
+        private void Initialize_ValidTestFile3_MikeLNF()
+        {
+            string path = System.Environment.CurrentDirectory;
+            path += @"\ValidFile3_MikeLNF.txt";
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Name: Stokes, Mike");
+                    sw.WriteLine("Money");
+                    sw.WriteLine("More Money");
+                    sw.WriteLine("Pizza");
+                }
+            }
+        }
+
+        private void Initialize_InvalidTestFile1_GarbageHeader()
+        {
+            string path = System.Environment.CurrentDirectory;
+            path += @"\InvalidFile1_GarbageHeader.txt";
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("GARBAGE HEADER");
+                    sw.WriteLine("Garbage Content");
+                    sw.WriteLine("Garbage Content");
+                    sw.WriteLine("Garbage Content");
+                }
+            }
+        }
+
+        [TestCleanup]
+        public void CleanUpTests()
+        {
+            string path = System.Environment.CurrentDirectory;
+            File.Delete(path + @"\ValidFile1_RayFNF.txt");
+            File.Delete(path + @"\ValidFile2_MarkFNF.txt");
+            File.Delete(path + @"\ValidFile3_MikeLNF.txt");
+            File.Delete(path + @"\InvalidFile1_GarbageHeader.txt");
+            Importer = null;
+        }
+
+        // ==============START OF TESTS==================
+
+        [TestMethod]
+        public void ReadInUser_ValidTestFile1_ReturnsValidUserObject()
+        {
+            (User user, List<Gift> gifts) = Importer.ReadInUser("ValidFile1_RayFNF.txt");
+
+            Assert.AreEqual("Raymond", user.FirstName);
+            Assert.AreEqual("Shiner", user.LastName);
+
+            Assert.AreEqual("PS4", gifts[0].Title);
+            Assert.AreEqual(1, gifts[0].Importance);
+            Assert.AreEqual("Money", gifts[1].Title);
+            Assert.AreEqual(2, gifts[1].Importance);
+            Assert.AreEqual("Peace on Earth", gifts[2].Title);
+            Assert.AreEqual(3, gifts[2].Importance);
+        }
+
+        [TestMethod]
+        public void ReadInUser_ValidTestFile2_ReturnsValidUserObject()
+        {
+            (User user, List<Gift> gifts)  = Importer.ReadInUser("ValidFile2_MarkFNF.txt");
+
+            Assert.AreEqual("Mark", user.FirstName);
+            Assert.AreEqual("Michaelis", user.LastName);
+
+            Assert.AreEqual("Fame", gifts[0].Title);
+            Assert.AreEqual(1, gifts[0].Importance);
+            Assert.AreEqual("Fortune", gifts[1].Title);
+            Assert.AreEqual(2, gifts[1].Importance);
+            Assert.AreEqual("Abs", gifts[2].Title);
+            Assert.AreEqual(3, gifts[2].Importance);
+        }
+
+        [TestMethod]
+        public void ReadInUser_ValidTestFile3_ReturnsValidUserObject()
+        {
+            (User user, List<Gift> gifts) = Importer.ReadInUser("ValidFile3_MikeLNF.txt");
+
+            Assert.AreEqual("Mike", user.FirstName);
+            Assert.AreEqual("Stokes", user.LastName);
+
+            Assert.AreEqual("Money", gifts[0].Title);
+            Assert.AreEqual(1, gifts[0].Importance);
+            Assert.AreEqual("More Money", gifts[1].Title);
+            Assert.AreEqual(2, gifts[1].Importance);
+            Assert.AreEqual("Pizza", gifts[2].Title);
+            Assert.AreEqual(3, gifts[2].Importance);
+        }
+
+        [TestMethod]
+        public void ReadInUser_InvalidTestFile1_ReturnsNullTuple()
+        {
+            (User user, List<Gift> gifts) = Importer.ReadInUser("InvalidFile1_GarbageHeader.txt");
+
+            Assert.IsNull(user);
+            Assert.IsNull(gifts);
+        }
+
+        [TestMethod]
+        public void ReadInUser_NonExistentTestFile_ReturnsNullTuple()
+        {
+            (User user, List<Gift> gifts) = Importer.ReadInUser("NonExistentFile.txt");
+
+            Assert.IsNull(user);
+            Assert.IsNull(gifts);
+        }
+
+        [TestMethod]
+        public void FileHeaderIsFormattedCorrectly_CorrectFormat_ReturnsTrue()
+        {
+            string header = "Name: Raymond Shiner";
+
+            bool res = Importer.FileHeaderIsFormattedCorrectly(header);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void FileHeaderIsFormattedCorrectly_CorrectFormatExtraWhiteSpaceOnEitherEnd_ReturnsTrue()
+        {
+            string header = "Name: Raymond Shiner    ";
+
+            bool res = Importer.FileHeaderIsFormattedCorrectly(header);
+            Assert.IsTrue(res);
+        }
+
+        [TestMethod]
+        public void FileHeaderIsFormattedCorrectly_NullEntry_ReturnsFalse()
+        {
+            bool res = Importer.FileHeaderIsFormattedCorrectly(null);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void FileHeaderIsFormattedCorrectly_GarbageHeaderLine_ReturnsFalse()
+        {
+            string header = "LEEROOOOOY JENKINS";
+            bool res = Importer.FileHeaderIsFormattedCorrectly(header);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void FileHeaderIsFormattedCorrectly_2CommasInHeader_ReturnsFalse()
+        {
+            string header = "Name: Shiner, Ray, Something";
+
+            bool res = Importer.FileHeaderIsFormattedCorrectly(header);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void FileHeaderIsFormattedCorrectly_3NamesInHeader_ReturnsFalse()
+        {
+            string header = "Name: Raymond Lawrence Shiner";
+
+            bool res = Importer.FileHeaderIsFormattedCorrectly(header);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void FileHeaderIsFormattedCorrectly_HeaderFirstPartCorrectNoName_ReturnsFalse()
+        {
+            string header = "Name: ";
+
+            bool res = Importer.FileHeaderIsFormattedCorrectly(header);
+            Assert.IsFalse(res);
+        }
+
+        [TestMethod]
+        public void ExtractNamesFromHeader_ValidHeaderFirstNameFirstRay_CorrectOutput()
+        {
+            var header = "Name: Raymond Shiner";
+
+            (string firstName, string lastName) = Importer.ExtractNamesFromHeader(header);
+
+            Assert.AreEqual("Raymond", firstName);
+            Assert.AreEqual("Shiner", lastName);
+        }
+
+        [TestMethod]
+        public void ExtractNamesFromHeader_ValidHeaderFirstNameFirstMike_CorrectOutput()
+        {
+            var header = "Name: Mike Stokes";
+
+            (string firstName, string lastName) = Importer.ExtractNamesFromHeader(header);
+
+            Assert.AreEqual("Mike", firstName);
+            Assert.AreEqual("Stokes", lastName);
+        }
+
+        [TestMethod]
+        public void ExtractNamesFromHeader_ValidHeaderLastNameFirstMark_CorrectOutput()
+        {
+            var header = "Name: Michaelis, Mark";
+
+            (string firstName, string lastName) = Importer.ExtractNamesFromHeader(header);
+
+            Assert.AreEqual("Mark", firstName);
+            Assert.AreEqual("Michaelis", lastName);
+        }
+
+        [TestMethod]
+        public void ExtractNamesFromHeader_ValidHeaderLastNameFirstMarkExtraWhiteSpace_CorrectOutput()
+        {
+            var header = "Name: Michaelis, Mark    ";
+
+            (string firstName, string lastName) = Importer.ExtractNamesFromHeader(header);
+
+            Assert.AreEqual("Mark", firstName);
+            Assert.AreEqual("Michaelis", lastName);
+        }
+
+        [TestMethod]
+        public void ExtractNamesFromHeader_InvalidHeaderGarbageHeader_ReturnsNullTuple()
+        {
+            var header = "GARBAGE HEADER";
+
+            (string firstName, string lastName) = Importer.ExtractNamesFromHeader(header);
+
+            Assert.IsNull(firstName);
+            Assert.IsNull(lastName);
+        }
+
+        [TestMethod]
+        public void ExtractNamesFromHeader_InValidHeader2Commas_ReturnsNullTuple()
+        {
+            var header = "Name: something, else,";
+
+            (string firstName, string lastName) = Importer.ExtractNamesFromHeader(header);
+
+            Assert.IsNull(firstName);
+            Assert.IsNull(lastName);
+        }
+    }
+}
---
+++ SecretSanta/SecretSanta.Import/FileImporter.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using SecretSanta.Domain.Models;
+using SecretSanta.Domain.Services;
+
+namespace SecretSanta.Import
+{
+    public class FileImporter
+    {
+        /// <summary>
+        /// Reads in User from given filename and returns a tuple of a User Object with FirstName and LastName
+        /// Stated in header file and a list of gifts populated with titles and importance. Returns (null, null) if file not found or file
+        /// incorrectly formatted.
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+
+        public (User user, List<Gift> gifts) ReadInUser(string filename)
+        {
+            string[] lines;
+
+            try
+            {
+                lines = System.IO.File.ReadAllLines(
+                    System.Environment.CurrentDirectory + @"\" + filename);
+            }
+
+            catch (Exception)
+            {
+                return (null, null);
+            }
+
+            (string firstname, string lastname) = ExtractNamesFromHeader(lines[0]);
+
+            if (firstname == null || lastname == null)
+            {
+                return (null, null);
+            }
+
+            User user = new User();
+            user.FirstName = firstname;
+            user.LastName = lastname;
+
+            List<Gift> gifts = new List<Gift>();
+
+            for (int x = 1; x < lines.Length; x++)
+            {
+                Gift theGift = new Gift();
+                theGift.Title = lines[x];
+                theGift.Importance = x;
+                gifts.Add(theGift);
+            }
+
+            return (user, gifts);
+        }
+
+        /// <summary>
+        /// Takes in the correctly formatted header file and returns tuple (firstname, lastname) of header file. If header formatted
+        /// incorrectly it will return tuple (null, null).
+        /// </summary>
+        /// <param name="header">Correctly formatted header file - 'Name: First Last' or 'Name: Last, First'</param>
+        /// <returns></returns>
+
+        public (string firstname, string lastname) ExtractNamesFromHeader(string header)
+        {
+            if (!FileHeaderIsFormattedCorrectly(header))
+            {
+                return (null, null);
+            }
+
+            string firstName;
+            string lastName;
+
+            var fullName = header.Substring(6); //Cutting off "Name: "
+
+
+            var names = fullName.Split(" ");
+
+            if (fullName.Contains(","))
+            {
+                lastName = names[0].Substring(0, names[0].Length - 1); //cut off , from end of lastname
+                firstName = names[1];
+            }
+
+            else
+            {
+                firstName = names[0];
+                lastName = names[1];
+            }
+
+            return (firstName, lastName);
+        }
+
+        /// <summary>
+        /// File is defined as being formatted correctly if Header Line is formatted correctly. As of right now
+        /// This library is assuming that every line in the file after the first line will contain just the title of each gift to add,
+        /// So it doesn't care about the format of header past the first.
+        /// </summary>
+        /// <param name="header"></param>
+        /// <returns></returns>
+
+        public bool FileHeaderIsFormattedCorrectly(string header)
+        {
+            if (header == null)
+                return false;
+
+            if (header.StartsWith("Name: "))
+            {
+                if (header.Trim().Split(",").Length > 2 || header.Trim().Split(" ").Length != 3) // length will always be three if no comma and string formatted correct
+                    return false;
+
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
