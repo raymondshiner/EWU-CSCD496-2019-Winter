daed1f9d02b78c9d143cd20698cff73f3222e466
Raymond Shiner <raymondshiner@gmail.com>
2019-01-30 00:36:09 -0800

    Implement Get/Create/Update/Delete User Objects and associated files and tests.

---
+++ SecretSanta/test/SecretSanta.Api.Tests/TestableUserService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SecretSanta.Domain.Models;
+using SecretSanta.Domain.Services;
+
+namespace SecretSanta.Api.Tests
+{
+    class TestableUserService : IUserService
+    {
+        public int GetUser_UserId { get; set; }
+        public User GetUser_Return { get; set; }
+
+        public User GetUser(int userId)
+        {
+            GetUser_UserId = userId;
+            return GetUser_Return;
+        }
+
+        public User AddUser_User { get; set; }
+        public User AddUser_Return { get; set; }
+
+
+        public User AddUser(User user)
+        {
+            AddUser_User = user;
+            return AddUser_Return;
+        }
+
+        public User UpdateUser_User { get; set; }
+        public User UpdateUser_Return { get; set; }
+
+        public User UpdateUser(User user)
+        {
+            UpdateUser_User = user;
+            return UpdateUser_Return;
+        }
+
+        public int DeleteUser_UserId { get; set; }
+
+        public void DeleteUser(int userId)
+        {
+            DeleteUser_UserId = userId;
+        }
+    }
+}
---
+++ SecretSanta/test/SecretSanta.Api.Tests/UserControllerTests.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SecretSanta.Api.Controllers;
+using SecretSanta.Domain.Models;
+
+namespace SecretSanta.Api.Tests
+{
+    [TestClass]
+    public class UserControllerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void UserController_RequiresUserService()
+        {
+            new UserController(null);
+        }
+
+        [TestMethod]
+        public void GetUser_ReturnsAUser()
+        {
+            var user = new User
+            {
+                Id = 4,
+                FirstName = "Raymond",
+                LastName = "Shiner"
+            };
+
+            var testService = new TestableUserService
+            {
+                GetUser_Return = user
+            };
+
+            var controller = new UserController(testService);
+            var result = controller.GetUser(4);
+            bool res = UserObjectsAreEqual(DTO.User.ToEntity(result.Value), user);
+
+            Assert.IsTrue(res);
+            Assert.AreEqual(4, testService.GetUser_UserId);
+        }
+
+        [TestMethod]
+        public void GetUser_NegativeUserIdReturnsNotFound()
+        {
+            var testService = new TestableUserService();
+            var controller = new UserController(testService);
+            var result = controller.GetUser(-1);
+
+            Assert.IsTrue(result.Result is NotFoundResult);
+            Assert.AreEqual(0, testService.GetUser_UserId);
+        }
+
+        [TestMethod]
+        public void CreateUser_AddsAUser()
+        {
+            var user = new User
+            {
+                Id = 4,
+                FirstName = "Raymond",
+                LastName = "Shiner"
+            };
+
+            var testService = new TestableUserService
+            {
+                AddUser_Return = user
+            };
+
+            var controller = new UserController(testService);
+            
+            var result = controller.CreateUser(new DTO.User(user));
+            bool res = UserObjectsAreEqual(DTO.User.ToEntity(result.Value), user);
+            bool serviceInvoked = UserObjectsAreEqual(user, testService.AddUser_User);
+
+            Assert.IsTrue(res);
+            Assert.IsTrue(serviceInvoked);
+        }
+
+        [TestMethod]
+        public void CreateUser_NullUserReturnsBadRequest()
+        {
+            var testService = new TestableUserService();
+            var controller = new UserController(testService);
+
+            var result = controller.CreateUser(null);
+
+            Assert.IsTrue(result.Result is BadRequestResult);
+        }
+
+        [TestMethod]
+        public void UpdateUser_UserIsUpdated()
+        {
+            var user = new User
+            {
+                Id = 4,
+                FirstName = "Raymond",
+                LastName = "Shiner"
+            };
+
+            var testService = new TestableUserService
+            {
+                UpdateUser_Return = user
+            };
+
+            var controller = new UserController(testService);
+
+            var result = controller.UpdateUser(4, new DTO.User(user));
+            bool res = UserObjectsAreEqual(DTO.User.ToEntity(result.Value), user);
+            bool serviceInvoked = UserObjectsAreEqual(user, testService.UpdateUser_User);
+
+            Assert.IsTrue(res);
+            Assert.IsTrue(serviceInvoked);
+        }
+
+        [TestMethod]
+        public void UpdateUser_NullUserReturnsBadRequest()
+        {
+            var testService = new TestableUserService();
+            var controller = new UserController(testService);
+
+            var result = controller.UpdateUser(1, null);
+
+            Assert.IsTrue(result.Result is BadRequestResult);
+        }
+
+        [TestMethod]
+        public void UpdateUser_NegativeUserIdReturnsNotFound()
+        {
+            var testService = new TestableUserService();
+            var controller = new UserController(testService);
+            var result = controller.UpdateUser(-1, new DTO.User());
+
+            Assert.IsTrue(result.Result is NotFoundResult);
+            Assert.IsNull(testService.UpdateUser_User);
+        }
+
+        [TestMethod]
+        public void DeleteUser_UserIsDeleted()
+        {
+            var testService = new TestableUserService();
+            var controller = new UserController(testService);
+
+            controller.DeleteUser(4);
+
+            Assert.AreEqual(4, testService.DeleteUser_UserId);
+        }
+
+        [TestMethod]
+        public void DeleteUser_NegativeUserIdReturnsNotFound()
+        {
+            var testService = new TestableUserService();
+            var controller = new UserController(testService);
+            var result = controller.DeleteUser(-1);
+
+            Assert.IsTrue(result is NotFoundResult);
+            Assert.AreEqual(0, testService.DeleteUser_UserId);
+        }
+
+        private bool UserObjectsAreEqual(User user1, User user2)
+        {
+            return user1.Id == user2.Id
+                   && user1.FirstName == user2.FirstName
+                   && user1.LastName == user2.LastName;
+        }
+    }
+}
