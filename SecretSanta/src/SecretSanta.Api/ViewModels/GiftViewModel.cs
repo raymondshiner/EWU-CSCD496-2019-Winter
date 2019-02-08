614a2fc0a255782c0447f8f1e6e2fcb46588fcd9
Raymond Shiner <raymondshiner@gmail.com>
2019-02-07 23:13:12 -0800

    Implement Database Level Requirements and add updated migration

--- SecretSanta/src/SecretSanta.Api/ViewModels/GiftViewModel.cs
+++ SecretSanta/src/SecretSanta.Api/ViewModels/GiftViewModel.cs
@@ -11,28 +11,12 @@ namespace SecretSanta.Api.ViewModels
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public int OrderOfImportance { get; set; }
+        public int? OrderOfImportance { get; set; }
         public string Url { get; set; }
 
         public GiftViewModel()
         {
 
-        }
-
-        public static GiftViewModel ToViewModel(Gift gift)
-        {
-            if (gift == null) throw new ArgumentNullException(nameof(gift));
-
-            var result = new GiftViewModel
-            {
-                Id = gift.Id,
-                Title = gift.Title,
-                Description = gift.Description,
-                OrderOfImportance = gift.OrderOfImportance,
-                Url = gift.Url,
-            };
-
-            return result;
         }
     }
 }
