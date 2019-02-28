39d43c86f23c2caa2f39e859718d0ecc4026222b
Raymond Shiner <raymondshiner@gmail.com>
2019-02-28 00:05:45 -0800

    Implement Add Groups

---
+++ SecretSanta/src/SecretSanta.Web/Controllers/GroupsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SecretSanta.Web.ApiModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SecretSanta.Web.Controllers
+{
+    public class GroupsController : Controller
+    {
+        private IHttpClientFactory ClientFactory { get; }
+        public GroupsController(IHttpClientFactory clientFactory)
+        {
+            ClientFactory = clientFactory;
+        }
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+            {
+                var secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                ViewBag.Groups = await secretSantaClient.GetGroupsAsync();
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(GroupInputViewModel viewModel)
+        {
+            IActionResult result = View();
+
+            if (ModelState.IsValid)
+            {
+                using (var httpClient = ClientFactory.CreateClient("SecretSantaApi"))
+                {
+                    try
+                    {
+                        var secretSantaClient = new SecretSantaClient(httpClient.BaseAddress.ToString(), httpClient);
+                        await secretSantaClient.CreateGroupAsync(viewModel);
+
+                        result = RedirectToAction(nameof(Index));
+                    }
+                    catch (SwaggerException se)
+                    {
+                        ViewBag.ErrorMessage = se.Message;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
